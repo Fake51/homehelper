a38cfde9fc7c25571256c00ad4fe697815fc8a96
Peter Lind <peter.e.lind@gmail.com>
2019-03-04 21:28:36 +0100

    switching to react-app

--- app/grid-block.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class GridBlock extends React.PureComponent {
-    render() {
-        return <div className='gridBlock'>{this.props.children}</div>;
-    }
-}
